2b6c2251ef7b2f41028b06777c7206bbddf0d4d1
seongho00 <tjdwns000927@gmail.com>
2025-03-04 09:53:29 +0900

    work_02_28

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+
+
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+
+class Main {
+    public static void main(String[] args) {
+/*      System.out.println("안녕하세요.1");
+        System.out.println("안녕하세요.2");
+        System.out.println("안녕하세요.3");
+        System.out.println("안녕하세요.4");
+        System.out.println("안녕하세요.5");
+        System.out.println("안녕하세요.6");
+        System.out.println("안녕하세요.7");
+        System.out.println("안녕하세요.8");
+        System.out.println("안녕하세요.9");
+        System.out.println("안녕하세요.10");  */
+//         같은 위상에 있는 코드는 같은 열로 맞춘다. 정렬 하는 법 : crtl + shift + f
+//         세미콜론(;) - 문장의 끝마침
+//         또다른 정답
+//         System.out.println("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.");
+        System.out.print("안녕\n"); //print 와 println 의 차이
+//         주황색 - 예악어, 키워드(이미 지정되어 있는 언어)
+//         주석 처리 방법 1. //  2. ctrl + /  3. ctrl + shift + /
+    }
+}
+
