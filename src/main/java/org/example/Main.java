351517679988dc681ee475b088f638f5d7315138
seongho00 <tjdwns000927@gmail.com>
2025-03-04 16:28:15 +0900

    03_04_work_09

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,84 +1,142 @@
 package org.example;
 
-// 문제 : 할인 대상인지 아닌지 출력해주세요.
-// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
-// 조건 : 출력예시 처럼 출력되어야 합니다.
-// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
-// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
-// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
+// 문제 : 아래와 같이 출력해주세요.
+
+//class Main {
+//    public static void main(String[] args) {
+//        System.out.println("8 * 1 = 8\n" +
+//                "8 * 2 = 16\n" +
+//                "8 * 3 = 24\n" +
+//                "8 * 4 = 32\n" +
+//                "8 * 5 = 40\n" +
+//                "8 * 6 = 48\n" +
+//                "8 * 7 = 56\n" +
+//                "8 * 8 = 64\n" +
+//                "8 * 9 = 72");
+//
+//        // 출력
+//    /*
+//    8 * 1 = 8
+//    8 * 2 = 16
+//    8 * 3 = 24
+//    8 * 4 = 32
+//    8 * 5 = 40
+//    8 * 6 = 48
+//    8 * 7 = 56
+//    8 * 8 = 64
+//    8 * 9 = 72
+//    */
+//
+//    }
+//}
+
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : dan 변수의 값에 따라 첫번째로 곱하는 수가 변하게 해주세요.
+
+//class Main {
+//    public static void main(String[] args) {
+//        int dan = 8;
+//
+//        System.out.println(dan + " * 1 = 8");
+//        System.out.println(dan + " * 2 = 16");
+//        System.out.println(dan + " * 3 = 24");
+//        System.out.println(dan + " * 4 = 32");
+//        System.out.println(dan + " * 5 = 40");
+//        System.out.println(dan + " * 6 = 48");
+//        System.out.println(dan + " * 7 = 56");
+//        System.out.println(dan + " * 8 = 64");
+//        System.out.println(dan + " * 9 = 72");
+//
+//        // 출력
+//    /*
+//    8 * 1 = 8
+//    8 * 2 = 16
+//    8 * 3 = 24
+//    8 * 4 = 32
+//    8 * 5 = 40
+//    8 * 6 = 48
+//    8 * 7 = 56
+//    8 * 8 = 64
+//    8 * 9 = 72
+//    */
+//    }
+//}
+
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : dan 변수의 값에 따라 그에 해당하는 구구단이 출력 되도록 해주세요.
+
+//class Main {
+//    public static void main(String[] args) {
+//        int dan = 9;
+//
+//        System.out.println(dan + " * 1 = " + dan * 1);
+//        System.out.println(dan + " * 2 = " + dan * 2);
+//        System.out.println(dan + " * 3 = " + dan * 3);
+//        System.out.println(dan + " * 4 = " + dan * 4);
+//        System.out.println(dan + " * 5 = " + dan * 5);
+//        System.out.println(dan + " * 6 = " + dan * 6);
+//        System.out.println(dan + " * 7 = " + dan * 7);
+//        System.out.println(dan + " * 8 = " + dan * 8);
+//        System.out.println(dan + " * 9 = " + dan * 9);
+//
+//        // 출력
+//    /*
+//    8 * 1 = 8
+//    8 * 2 = 16
+//    8 * 3 = 24
+//    8 * 4 = 32
+//    8 * 5 = 40
+//    8 * 6 = 48
+//    8 * 7 = 56
+//    8 * 8 = 64
+//    8 * 9 = 72
+//    */
+//    }
+//}
+
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int age = 19; // 이 값을 바꿔가면서 실행해보세요.
+        int dan = 8;
 
-        System.out.println("당신의 나이는 " + age + "살 입니다.");
+        // 수정가능지역 시작
+        int i = 1;
 
-        // 구현시작
-        // 1번째 방법
-        if (age <= 19 || age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else
-            System.out.println("할인 대상이 아닙니다.");
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i += 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
 
-        // 2번째 방법
-        if (age <= 19) {
-            System.out.println("할인 대상입니다.");
-        }
-        if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        }
-        if (age >= 60) {
-            System.out.println("할인 대상입니다.");
-        }
 
-        // 3번째 방법
-        if (age <= 19) {
-            System.out.println("할인 대상입니다.");
-        } else if (age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else
-            System.out.println("할인 대상이 아닙니다.");
 
-        // 4번째 방법
-        if (age <= 19) {
-            System.out.println("할인 대상입니다.");
-        } else if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        } else if (age >= 60) {
-            System.out.println("할인 대상입니다.");
-        }
+        // 수정가능지역 끝
 
-        // 5번쨰 방법
-        if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        } else
-            System.out.println("할인 대상입니다.");
-
-        // 6번째 방법
-        if (age <= 19 || age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        }
-
-        // 7번째 방법
-        if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        } else if (age <= 19 || age >= 60) {
-            System.out.println("할인 대상입니다.");
-        }
-
-        // 8번째 방법
-        if (age <= 19) {
-            System.out.println("할인 대상입니다.");
-        } else if (age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        } else if (age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else if (age > 19) {
-            System.out.println("할인 대상이 아닙니다.");
-        }
-
-        // 출력 => 할인대상입니다. 또는 할인 대상이 아닙니다.
+        // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
     }
 }
