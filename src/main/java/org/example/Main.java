8b1e43b0dd531d69b0e27e5f7bb73d5e4e4e75fb
seongho00 <tjdwns000927@gmail.com>
2025-03-04 10:05:23 +0900

    commit_test_1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,6 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-/*      System.out.println("안녕하세요.1");
-        System.out.println("안녕하세요.2");
-        System.out.println("안녕하세요.3");
-        System.out.println("안녕하세요.4");
-        System.out.println("안녕하세요.5");
-        System.out.println("안녕하세요.6");
-        System.out.println("안녕하세요.7");
-        System.out.println("안녕하세요.8");
-        System.out.println("안녕하세요.9");
-        System.out.println("안녕하세요.10");  */
-//         같은 위상에 있는 코드는 같은 열로 맞춘다. 정렬 하는 법 : crtl + shift + f
-//         세미콜론(;) - 문장의 끝마침
-//         또다른 정답
-//         System.out.println("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.");
         System.out.print("안녕\n"); //print 와 println 의 차이
 //         주황색 - 예악어, 키워드(이미 지정되어 있는 언어)
 //         주석 처리 방법 1. //  2. ctrl + /  3. ctrl + shift + /
