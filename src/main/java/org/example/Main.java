a31a9969e87ab67089eb4faf65b38a324d0adf2e
seongho00 <tjdwns000927@gmail.com>
2025-03-05 15:05:50 +0900

    03_05_pro_02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,15 @@
 package org.example;
 
-// 문제 : 1부터 5까지 출력해주세요.
+
+//모든 문제 for 로 다시 풀기
+
+//문제 : 1부터 5까지 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
 
-        int i = 1;
-
-        while (i <= 5) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
-            i++;
         }
 
     }
@@ -19,37 +19,11 @@ class Main {
 
 class Main {
     public static void main(String[] args) {
-        int i = -100;
 
-        while (i <= 25) {
+        for (int i = -100; i <=25; i++){
             System.out.println(i);
-            i += 1;
         }
 
-        // 출력
-    }
-}
-
-
-// 문제 : 아래와 같이 출력해주세요.
-// 문제 : 단을 `* 1000`부터 `* -500`까지 출력해주세요.
-
-class Main {
-    public static void main(String[] args) {
-        int dan = 8;
-
-        int i = 1000;
-        while (i >= -500) {
-            System.out.println(dan + " * " + i + " = " + dan * i);
-            i--;
-        }
-
-        // 출력
-    /*
-    8 * 1000 = 8000
-    ...
-    8 * -500 = -4000
-    */
     }
 }
 
@@ -59,11 +33,22 @@ class Main {
     public static void main(String[] args) {
         int dan = 8;
 
-        int i = 1000;
+        for (int i = 1000; i >= 1; i--) {
+            System.out.println(dan + " *  " + i + " = " + dan * i);
+        }
 
-        while (i >= 1) {
-            System.out.println(dan + " * " + i + " = " + dan * i);
-            i--;
+    }
+}
+
+// 문제 : 단을 `* 1000`부터 `* -500`까지 출력해주세요.
+
+
+class Main {
+    public static void main(String[] args) {
+        int dan = 8;
+
+        for (int i = 1000; i >= -500; i--) {
+            System.out.println(dan + " *  " + i + " = " + dan * i);
         }
 
     }
@@ -73,13 +58,10 @@ class Main {
 
 class Main {
     public static void main(String[] args) {
-
-        int i = 1;
         int sum = 0;
 
-        while (i <= 5) {
+        for (int i = 1; i <=5; i++){
             sum += i;
-            i++;
         }
 
         System.out.println(sum);
@@ -91,35 +73,26 @@ class Main {
 
 class Main {
     public static void main(String[] args) {
+        int sum = 0;
 
-        int i = -100;
-        int j = 0;
-
-        while (i <= 25) {
-            j += i;
-            i++;
+        for (int i =-100; i <=25;i++){
+            sum += i;
         }
 
-        System.out.println(j);
+        System.out.println(sum);
+
+
     }
 }
 
-//문제 1부터 3까지 출력하는 작업을 10번 해주세요 (2중 while문 사용)
+// 문제 1부터 3까지 출력하는 작업을 10번 해주세요 (2중 for문 사용)
 
 class Main {
     public static void main(String[] args) {
-
-        int i = 1;
-        int j = 1;
-
-        while (i <= 10) {
-            while (j <= 3) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 3;j++){
                 System.out.println(j);
-                j++;
             }
-            j = 1;
-            i++;
         }
-
     }
 }
