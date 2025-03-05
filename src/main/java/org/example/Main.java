986897eb65588c0d225b6ec476cbd3994c53f0d8
seongho00 <tjdwns000927@gmail.com>
2025-03-05 16:46:27 +0900

    03_05_work_05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,82 +1,35 @@
 package org.example;
 
-
-class Main {
-    public static void main(String[] args) {
-        int i = 1;
-
-        while (i <= 10) {
-            int k = 1; // while문 안쪽에 있지 않으면 k가 4로 고정 떄문에 1번만 반복하게 됨.
-            while (k <= 3) {
-                System.out.println(k);
-                k++;
-            }
-            i++;
-        }
-
-
-    }
-}
-
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
 
-        int rows = 4;
+        char c = 'a';
+        System.out.println(c);
+        System.out.printf("%c\n", c); // %c : 문자열 서식 지정자
 
-        for (int i = 1; i <= rows; i++) {
-            for (int k = 1; k <= i; k++) { // 조건식에 i가 들어갈 수도 있음.
-                System.out.print("* ");
-            }
-            System.out.println("");
-        }
-        // 무슨 일이 발생하는지 글로 적기
-        // 우선 i가 들어간 for문을 for_i, k가 들어간 for문을 for_k라 명명.
-        // for_i 문에서 i가 1에서 시작된다.
-        // for_i 문에서 i 가 1일 경우 for_k은 i가 1이기 떄문에 1번 반복한다.
-        // 즉, "* "가 한 번 출력되고 for_k문을 나온다.
-        // 그 후 for_i 문에 있는 ""(줄넘김)이 출력된 후 i 가 1증가한다.(i = 2)
-        // 다시 for_i문이 작동하하는데 이는 i가 보다 row, 즉 4값보다 작기 때문에 for_i문을 실행한다.
-        // i가 2인 상태에서 for_k문이 출력된다. 이 때, for_k문은 i보다 작거나 같은만큼 실행되기 떄문에
-        // 2번 실행되며 "* "문자가 2번 출력된다
-        // 이를 반복하여 i 가 row값인 4보다 클때까지 반복하여 (총 4번) 출력하게 된다.
+        int a = 97;
 
-
+        System.out.printf("%d\n", a); // %d : 정수형 서식 지정자
+        System.out.printf("%c\n", a); // 출력 : a
+        // %d %f %c %s 정도만 알고 있기
+        // %c : 정수를 문자로 -> 97: a / 65 : A / 98 : b 정도 알고 있기
     }
 }
-
 
 class Main {
     public static void main(String[] args) {
 
-        // 2번
-        int n = 4;
+        Scanner sc = new Scanner(System.in); //입력하는 참조타입
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        int num = sc.nextInt(); // 정수 입력 가능
+        // 정수를 입력하지 않을 경우 , inputmismatchexception 오류 뜸
 
+        String s = sc.nextLine();
 
+        System.out.println(num);
 
-
-
-        //3번
-        for (int i = n-1; i >= 1; i--) {
-            for (int k = n; k > i; k--) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
 
     }
 }
-
