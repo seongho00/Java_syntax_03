887f6a27e7002a81bfc14899708be77a1d3e09bd
seongho00 <tjdwns000927@gmail.com>
2025-03-05 16:05:17 +0900

    03_05_work_04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,98 +1,82 @@
 package org.example;
 
 
-//모든 문제 for 로 다시 풀기
-
-//문제 : 1부터 5까지 출력해주세요.
-
 class Main {
     public static void main(String[] args) {
+        int i = 1;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-
-    }
-}
-
-// 문제 : -100부터 25까지 출력해주세요.
-
-class Main {
-    public static void main(String[] args) {
-
-        for (int i = -100; i <=25; i++){
-            System.out.println(i);
-        }
-
-    }
-}
-
-// 문제 : 단을 `* 1000`부터 `* 1'까지 출력해주세요.
-
-class Main {
-    public static void main(String[] args) {
-        int dan = 8;
-
-        for (int i = 1000; i >= 1; i--) {
-            System.out.println(dan + " *  " + i + " = " + dan * i);
-        }
-
-    }
-}
-
-// 문제 : 단을 `* 1000`부터 `* -500`까지 출력해주세요.
-
-
-class Main {
-    public static void main(String[] args) {
-        int dan = 8;
-
-        for (int i = 1000; i >= -500; i--) {
-            System.out.println(dan + " *  " + i + " = " + dan * i);
-        }
-
-    }
-}
-
-// 문제 1부터 5까지의 합을 출력해주세요.
-
-class Main {
-    public static void main(String[] args) {
-        int sum = 0;
-
-        for (int i = 1; i <=5; i++){
-            sum += i;
-        }
-
-        System.out.println(sum);
-    }
-}
-
-
-// 문제 -100부터 25까지의 합을 출력해주세요.
-
-class Main {
-    public static void main(String[] args) {
-        int sum = 0;
-
-        for (int i =-100; i <=25;i++){
-            sum += i;
-        }
-
-        System.out.println(sum);
-
-
-    }
-}
-
-// 문제 1부터 3까지 출력하는 작업을 10번 해주세요 (2중 for문 사용)
-
-class Main {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 3;j++){
-                System.out.println(j);
+        while (i <= 10) {
+            int k = 1; // while문 안쪽에 있지 않으면 k가 4로 고정 떄문에 1번만 반복하게 됨.
+            while (k <= 3) {
+                System.out.println(k);
+                k++;
             }
+            i++;
         }
+
+
     }
 }
+
+
+class Main {
+    public static void main(String[] args) {
+
+        int rows = 4;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int k = 1; k <= i; k++) { // 조건식에 i가 들어갈 수도 있음.
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        // 무슨 일이 발생하는지 글로 적기
+        // 우선 i가 들어간 for문을 for_i, k가 들어간 for문을 for_k라 명명.
+        // for_i 문에서 i가 1에서 시작된다.
+        // for_i 문에서 i 가 1일 경우 for_k은 i가 1이기 떄문에 1번 반복한다.
+        // 즉, "* "가 한 번 출력되고 for_k문을 나온다.
+        // 그 후 for_i 문에 있는 ""(줄넘김)이 출력된 후 i 가 1증가한다.(i = 2)
+        // 다시 for_i문이 작동하하는데 이는 i가 보다 row, 즉 4값보다 작기 때문에 for_i문을 실행한다.
+        // i가 2인 상태에서 for_k문이 출력된다. 이 때, for_k문은 i보다 작거나 같은만큼 실행되기 떄문에
+        // 2번 실행되며 "* "문자가 2번 출력된다
+        // 이를 반복하여 i 가 row값인 4보다 클때까지 반복하여 (총 4번) 출력하게 된다.
+
+
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+
+        // 2번
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+        //3번
+        for (int i = n-1; i >= 1; i--) {
+            for (int k = n; k > i; k--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
