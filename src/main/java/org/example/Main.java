0f7bee23411459ed365a8630edd0f3c42e874135
seongho00 <tjdwns000927@gmail.com>
2025-03-06 11:43:40 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,6 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in); //입력하는 참조타입
-
-        int num = sc.nextInt(); // 정수 입력 가능
-        // 정수를 입력하지 않을 경우 , inputmismatchexception 오류 뜸
-
-        String s = sc.nextLine();
-
-        System.out.println(num);
-
 
     }
 }
