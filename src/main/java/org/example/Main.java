8f6cf277a52e891a078616376f02f9b7578ef114
seongho00 <tjdwns000927@gmail.com>
2025-03-06 09:21:31 +0900

    03_06_work_01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,6 @@ package org.example;
 
 import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) {
-
-        char c = 'a';
-        System.out.println(c);
-        System.out.printf("%c\n", c); // %c : 문자열 서식 지정자
-
-        int a = 97;
-
-        System.out.printf("%d\n", a); // %d : 정수형 서식 지정자
-        System.out.printf("%c\n", a); // 출력 : a
-        // %d %f %c %s 정도만 알고 있기
-        // %c : 정수를 문자로 -> 97: a / 65 : A / 98 : b 정도 알고 있기
-    }
-}
 
 class Main {
     public static void main(String[] args) {
