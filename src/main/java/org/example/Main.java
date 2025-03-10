8857ed8d25b78805fa1c8bfaf02d32c3fd9fea0b
seongho00 <tjdwns000927@gmail.com>
2025-03-10 12:51:01 +0900

    03_10_work_01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,45 +8,59 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //arr1 변수에 [true, false, true] 연결
-        boolean[] arr1 = new boolean[3];
-        arr1[0] = true;
-        arr1[1] = false;
-        arr1[2] = true;
+        int person2_num = 1;
+        int person2_age = 15;
+        int person2_height = 170;
+        // 변수 이름으로 정보 전달
+        // Stack영역 메모리를 많이 잡아먹음
 
 
-        //arr2 변수에 [3.14, 7.77, 11.11] 연결
-        float[] arr2 = new float[3];
-        arr2[0] = 3.14f;
-        arr2[1] = 7.77f;
-        arr2[2] = 11.11f;
+        int[] person1 = new int[3];
+        person1[0] = 1;
+        person1[1] = 12;
+        person1[2] = 160;
+        // 변수이름을 통한 정보전달이 안 됨. + 실수 & 문장 표현도 힘듬(타입의 다양성)
+        // 만들어진 객체 : 인스턴스 / 객체 안의 변수처럼 동작하는 공간 : 인스턴스 변수
+        // 클래스 == 설계도 == 인스턴스 설계도 == 객체 설계도
 
-        //arr 변수에 [1~10] 연결
-        int[] arr3 = new int[10];
-
-        for (int i = 0; i < arr3.length; i++) {
-            arr3[i] = i + 1;
-        }
-
-        int[] sum = {arr1.length, arr2.length, arr3.length};
+        // 지역변수의 죽음 : 함수의 끝
+        // 인스턴스 변수의 죽음 : 인스턴스 끝
+        // 인스턴스(객체)의 죽음 : 고립될 때
 
 
-        for (int i = 0; i < arr1.length; i++) {
-            System.out.print(arr1[i] + " ");
-        }
-        System.out.println("\n===========");
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.print(arr2[i] + " ");
-        }
-        System.out.println("\n===========");
-        for (int i = 0; i < arr3.length; i++) {
-            System.out.print(arr3[i] + " ");
-        }
-        // 이 3개를 반복문으로 묶고 싶은데
+        // 사용자 정의 객체 만들기 -> 객체 설게도 필요 = 클래스
+        철수 a = new 철수(); // 객체 이름 : 철수 / 변수 a
+        a.이름 = "김철수"; // a를 통해 철수라는 객체 안의 이름이라는 변수 안에 김철수를 넣음.
+        a.나이 = 23;
+        a.키 = 170.5;
+
+        // 예시
+        Scanner sc = new Scanner(System.in);
+
+
+        int[] arr = new int[7];
+        // 1차원 배열
+        arr[0] = 1;
+        arr[1] = 2;
+        //...
+
+
+        int[][] arr2 = new int[2][3];
+        // 2차원 배열  / 3개씩 2묶음
+        arr2[0][0] = 1;
+        arr2[0][1] = 2;
+        //...
+        arr2[1][2] = 6;
+
 
     }
 }
 
+class 철수 {
+    String 이름; // 객체 안에 '이름'이라는 공간을 만듬
+    int 나이;
+    double 키;
+}
 
 
 
