6098a3e7e614208e90d28cc74e49cc5bd82468b8
seongho00 <tjdwns000927@gmail.com>
2025-03-13 11:01:47 +0900

    9th_pro_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,147 +7,65 @@ import java.util.Scanner;
 import java.awt.*;
 import java.util.Arrays;
 
-// 상황 : DVD방에 3개의 서로다른 tv가 있다. 영업을 하다보니 각 tv의 특별한 기능은 거의 사용될 일이 없고 기본적인 켜기/끄기 만 사용된다. 현재 리모콘이 3개나 있는습니다.
-// 문제 : 알바를 편하게 하기 위해서 표준Tv 리모콘을 구현해주세요.
+
+// 문제 : 정수 i가 가지고 있는 10을 double 형 변수에 넣고 해당 변수의 값을 다시 i에 넣는 코드를 작성해주세요.
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("== 표준Tv 리모콘 들여오기 전 ==");
-
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
-
-        a샤오미Tv.켜기();
-        // 출력 => 샤오미Tv 켜집니다.
-        a샤오미Tv.끄기();
-        // 출력 => 샤오미Tv 꺼집니다.
-        a샤오미Tv.vr켜기();
-        // 출력 => 샤오미Tv vr켜기!
-
-        a삼성Tv.켜기();
-        // 출력 => 삼성Tv 켜집니다.
-        a삼성Tv.끄기();
-        // 출력 => 삼성Tv 꺼집니다.
-        a삼성Tv.ar켜기();
-        // 출력 => 삼성Tv ar켜기!
-
-        aLGTv.켜기();
-        // 출력 => LGTv 켜집니다.
-        aLGTv.끄기();
-        // 출력 => LGTv 꺼집니다.
-        aLGTv.게임모드전환();
-        // 출력 => LGTv 게임모드전환!
-
-        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
-
-        // 표준Tv 리모콘을 만든다.
-        표준Tv a표준Tv;
-
-        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
-        a표준Tv = a샤오미Tv;
-        a표준Tv.켜기();
-        // 출력 : 샤오기TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 샤오기TV 꺼집니다.
-
-        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
-        a표준Tv = a삼성Tv;
-        a표준Tv.켜기();
-        // 출력 : 삼성TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 삼성TV 꺼집니다.
-
-        // a표준Tv 변수에 LGTv 객체를 연결한다.
-        a표준Tv = aLGTv;
-        a표준Tv.켜기();
-        // 출력 : LGTV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : LGTV 꺼집니다.
-
-        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
-        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
-        LGTv aLGTv2 = (LGTv) a표준Tv;
-        aLGTv2.게임모드전환();
+        int i = 10;
+        double d = i; // 여기선 자동형변환 허용
+        i = (int) d; // 여기선 자동형변환 불가능
     }
 }
 
-class 표준Tv {
-    void 켜기() {
-
-    }
-
-    void 끄기() {
-
-    }
-}
-
-class 샤오미Tv extends 표준Tv {
-
-    void 켜기() {
-        System.out.println("샤오미Tv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("샤오미Tv 꺼집니다.");
-    }
-
-    void vr켜기() {
-        System.out.println("샤오미Tv vr켜기!");
-    }
-}
-
-class 삼성Tv extends 표준Tv {
-    void 켜기() {
-        System.out.println("삼성Tv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("삼성Tv 꺼집니다.");
-    }
-
-    void ar켜기() {
-        System.out.println("삼성Tv ar켜기!");
-    }
-}
-
-class LGTv extends 표준Tv {
-    void 켜기() {
-        System.out.println("LGTv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("LGTv 꺼집니다.");
-    }
-
-    void 게임모드전환() {
-        System.out.println("LGTv 게임모드전환!");
-    }
-}
-
-
-// 문제 : `a무기.공격();` 가 실행되는 세부과정을 자세히 쓰세요.
+// 문제 : 자동차 리모콘이 페라리 객체를 가리키게 한 후 해당 리모콘이 가리키고 있는 객체를 다시 페라리 리모콘으로 가리키게(참조하게) 하는 코드를 작성해주세요.
 
 class Main {
     public static void main(String[] args) {
-        무기 a무기 = new 칼();
-        // 무기 타입 객체와만 연결할 수 있는 변수 a무기에
-        // 칼 설계도로 만든 새로운 객체와 연결할 수 있는 리모콘을 준다.
-        // 이때, 칼 설계도는 무기 설계도와 같은 모양을 하기 위해 형변환이 이루어지며
-        // 무기 설계도에는 함수가 없기 떄문에 공격() 함수가 사라진다.
+        // 수정가능지역 시작
+        자동차 a자동차;
+        페라리 b자동차 = new 페라리();
 
-        // 무기 설계도 처럼 쓰인 칼은 공격 함수가 사라졌기 떄문에 함수를 사용할 수 없다.
-        a무기.공격();
+        a자동차 = b자동차;
+        a자동차.뚜껑이_열리다();
+
+        // 수정가능지역 끝
     }
 }
 
-class 무기 {
+class 자동차 {
+    void 달리다() {
+    }
 
-}
-
-class 칼 extends 무기 {
-    void 공격() {
+    void 서다() {
+    }
+    void 뚜껑이_열리다() {
     }
 }
 
+class 페라리 extends 자동차 {
+    void 뚜껑이_열리다() {
+    }
+}
+
+// 문제 : 아래가 실행되도록 해주세요.
+
+class Main {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+
+        a사람.나이 = 20;
+        a사람.a왼팔 = new 팔();
+    }
+}
+
+class 사람 {
+    // 클래스안에 변수를 정의 할 수 있다.
+    int 나이;
+    팔 a왼팔;
+}
+
+class 팔 {
+
+}
 
