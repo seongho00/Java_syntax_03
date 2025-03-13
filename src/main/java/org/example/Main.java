a553c1dcda7791c5526b14f9ee207355d5c84408
seongho00 <tjdwns000927@gmail.com>
2025-03-13 14:57:34 +0900

    9th_work_02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,96 +9,202 @@ import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        페라리 a페라리 = new 페라리();
+        사람 a사람 = new 사람();
+        a사람.나이 = 20;
+        a사람.a팔 = new 팔(); // 객체 생성 주소를 넣기 위한 변수 이므로
 
-        자동차 a자동차;
-
-        // 자동형변환, 자동캐스팅
-        // 자바가 판단하기에 안전한 경우만 자동형변환이 가능하다.
-        // 리모콘의 버튼 개수를 줄이는 경우는 언제나 안전하다.
-        a자동차 = a페라리;
-
-        페라리 b페라리 = a자동차; // a자동차가 무슨 타입인지 컴파일러는 모른다.
-        // a자동차가 페라리 클래스가 아닐 수도 있기 때문에 에러 발생.
-
-        // 아래에서는 `a자동차`변수에 들어있던 리모콘에 `뚜껑이_열리다` 버튼이 추가된다.
-        // 자바에서는 리모콘에 버튼이 빠지는건 상관하지 않지만 추가되는건 굉장히 무서워 한다.
-        // 왜냐하면 추가된 버튼에 해당된 기능이 연결된 객체에 없을 수도 있기 때문이다. (위의 설명)
-        // 그렇기 때문에 여기서는 무조건 수동형변환을 사용한다.
-        // 수동형변환을 사용하는 이유는 자바에게 개발자의 의도(이게 실수가 아니라는 뜻)를 명확히 밝히는 것과 같다.
-        페라리 a페라리2 = (페라리)a자동차;
-
-        a페라리2.뚜껑이_열리다(); // 페라리 타입으로 되었기 때문에 함수가 추가됨?
+        a사람.a팔.길이 = 10; // 타고 타고 가서 넣어야함.
     }
 }
-class 자동차 {
-    void 달리다() {}
-    void 서다() {}
+
+class 사람 {
+    int 나이;
+    팔 a팔; // 타입은 '팔'이 될 수밖에 없음.
 }
-class 페라리 extends 자동차 {
-    void 뚜껑이_열리다() {}
+
+class 팔 {
+    int 길이;
+}
+
+// 문제 : 아래가 실행되도록 해주세요.
+
+class Main {
+    public static void main(String[] args) {
+        전사 a전사 = new 전사();
+
+        a전사.나이 = 20;
+        a전사.a무기 = new 활();
+        a전사.a무기 = new 칼();
+
+    }
+}
+
+class 전사 {
+    int 나이;
+    무기 a무기;
+}
+
+class 무기 {
+
+}
+
+class 활 extends 무기 {
+
+}
+
+class 칼 extends 무기 {
+
 }
 
 
-//올바른 리턴타입으로 메서드를 만들어주세요
+// 문제 : 아래가 실행되도록 해주세요.
+
+class Main {
+    public static void main(String[] args) {
+        전사 a전사 = new 전사();
+
+        String 이름 = "칸";
+        a전사.이름 = 이름;
+        a전사.나이 = 20;
+        a전사.자기소개();
+
+        a전사.나이++;
+        a전사.자기소개();
+
+        a전사.나이 = 30;
+        a전사.이름 = "카니";
+        a전사.자기소개();
+
+        a전사.a무기 = new 활();
+        a전사.공격();
+        // 출력 : 카니가 활로 공격합니다.
+
+        a전사.a무기 = new 칼();
+        a전사.공격();
+        // 출력 : 카니가 칼로 공격합니다.
+
+        a전사.a무기 = new 칼();
+        a전사.이름 = "철수";
+        a전사.공격();
+
+    }
+}
+
+class 전사 {
+    // 인스턴스 변수
+    String 이름;
+    // 인스턴스 변수
+    int 나이;
+    // 인스턴스 변수
+    무기 a무기;
+
+
+    void 자기소개() {
+        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
+    }
+
+    void 공격() {
+        a무기.작동(this.이름); // 변수 넣는거 익숙해지기
+    }
+}
+
+class 무기 {
+    void 작동(String name) {
+    }
+
+}
+
+class 칼 extends 무기 {
+    void 작동(String name) {
+        System.out.println(name + "가 칼로 공격합니다.");
+    }
+
+}
+
+class 활 extends 무기 {
+    void 작동(String name) {
+        System.out.println(name + "가 활로 공격합니다.");
+    }
+
+}
+
+
+// 아래의 코드가 실행되도록 해주세요
+
 public class Main {
     public static void main(String[] args) {
-        int i = 로봇.get정수();
-        boolean b = 로봇.get논리();
-        사람 a사람 = 로봇.get사람();
-        사람 a사람2 = 로봇.get사람2();
-        사람 a사람3 = 로봇.get사람3();
-        사람 a사람4 = 로봇.get사람4();
-        사람 a사람5 = 로봇.get사람5(123, false);
-        로봇.get사람6(123, false); // 아무것도 하는 게 없는 코드
+        전사 a전사 = new 전사();
+
+        String 이름 = "칸";
+        a전사.이름 = 이름;
+        a전사.나이 = 20;
+        a전사.자기소개();
+
+        a전사.나이++;
+        a전사.자기소개();
+
+        a전사.나이 = 30;
+        a전사.이름 = "진";
+        a전사.자기소개();
+
+        a전사.a무기 = new 활();
+        a전사.공격();
+        //출력 : 진이 활로 공격합니다.
+
+        a전사.a무기 = new 칼();
+        a전사.공격();
+        //출력 : 진이 칼로 공격합니다.
+    }
+}
+
+class 전사 {
+    //인스턴스 변수
+    String 이름;
+    int 나이;
+    무기 a무기;
+
+    void 자기소개() {
+        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
+    }
+
+    void 공격() {
+        System.out.println(this.이름 + "이 " + this.a무기.공격() + "로 공격합니다.");
+    }
+
+    void 공격2() {
+
+        a무기.공격자이름 = this.이름;
+        a무기.공격2();
+    }
+}
+
+class 무기 {
+    String 공격자이름;
+
+    String 공격() {
+        return "";
+    }
+
+    void 공격2() {
 
     }
 }
 
-class 로봇 {
-    static int get정수() {
-        return 0;
+class 칼 extends 무기 {
+    String 공격() {
+        return "칼";
     }
 
-    static boolean get논리() {
-        return true;
+    void 공격2() {
+        System.out.println(공격자이름 + "이 칼로 공격합니다.");
     }
-
-    static 사람 get사람() {
-        return new 사람(); // 아무거나 리턴해줘도 됨.
-        // return null; 가능
-        
-    }
-
-    static 사람 get사람2() {
-        사람 a사람 = null;
-        return a사람; // null이 출력됨.
-    }
-
-    static 사람 get사람3() {
-        사람 a사람 = new 사람();
-        return a사람; // 객체 생성 및 연결
-    }
-
-    static 사람 get사람4() {
-        return new 사람();
-    }
-
-    static 사람 get사람5(int 학번, boolean 출결) {
-        사람 a사람 = new 사람();
-        a사람.학번 = 학번;
-        a사람.출결 = 출결;
-        return a사람; // 값이 채워진 객체와 연결시킴.
-    }
-
-    static void get사람6(int 학번, boolean 출결) {
-
-    }
-
 }
 
-class 사람 extends 로봇 {
-    int 학번;
-    boolean 출결;
+class 활 extends 무기 {
+    String 공격() {
+        return "활";
+    }
 }
+
+
 
