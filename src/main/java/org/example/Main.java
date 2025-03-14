477ab8409624a9b1fd2e1d48e4c791e721f2ce22
seongho00 <tjdwns000927@gmail.com>
2025-03-14 10:47:00 +0900

    10th_work_01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,60 +1,75 @@
 package org.example;
 
+import javax.sound.midi.Soundbank;
+import javax.swing.*;
 import java.io.CharArrayReader;
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
 import java.awt.*;
 import java.util.Arrays;
-
-//생성자
+import java.util.ArrayList;
 
 class Main {
     public static void main(String[] args) {
-        사람 사람1 = new 사람();
-        사람1.이름 = "김철수";
-        사람1.나이 = 22;
-        System.out.println("===사람1 정보===");
-        System.out.println("이름 : " + 사람1.이름); // 김철수
-        System.out.println("나이 : " + 사람1.나이); // 22
 
-        사람 사람2 = new 사람("이영희", 33);
-        System.out.println("===사람2 정보===");
-        System.out.println("이름 : " + 사람2.이름); // 이영희
-        System.out.println("나이 : " + 사람2.나이); // 22
-        
-        사람 사람3 = new 김철수();
-        System.out.println("===사람1 정보===");
-        System.out.println("이름 : " + 사람3.이름); // 김철수
-        System.out.println("나이 : " + 사람3.나이); // 22
+//        int lastID = 0; // 클래스쪽에 집어넣기
+//        Article a1 = new Article();
+//        lastID++;
+//        a1.id = lastID;
+//        System.out.println(a1.id);
+//
+//
+//        Article a2 = new Article();
+//        lastID++;
+//        a2.id = lastID;
+//
+//
+//        Article a3 = new Article(2, "2025-03-14 12:12:12");
+//        lastID++;
+//        a3.id = lastID;
+
+        Article a1 = new Article();
+
+        Article a2 = new Article();
+
+        Article[] articles = new Article[3]; // 게시글 3개 다룬다
+
+        articles[0] = a1;
+        articles[1] = a2;
+        System.out.println(articles[0].id);
+
+        articles[2] = new Article(); // 배열로 한번에 여러 객체를 다룬다.
+
+
     }
 }
 
-class 사람 {
-    String 이름;
-    int 나이;
-    // int 나이 = 22;
-    
-    사람(String name, int age) {
-        this.이름 = name;
-        this.나이 = age;
-    } // 오버로딩 : 다형성
-    
-    
-    사람() {
-        this.나이 = 22; // 나이 22로 고정
-        System.out.println("사람 생성자 실행됨"); // 부모가 먼저 실행 / 부모만 있어도 실행됨.
-    } // 기본생성자
+class Article {
+    static int lastId; // 클래스변수(정적변수) (클래스에 속함) : 모든 인스턴스가 공유함
+    //객체생성과 무관 /공공자원
 
-}
+    static { //static 전용 생성자 / 프로그램 시작할 때 바로 작동
+        lastId = 0;
+    }
 
-class 김철수 extends 사람{
-    김철수() {
-        System.out.println("김철수 생성자 실행됨"); // 그 후 자식 생성자 실행 / 자식만 있어도 실행됨.
-        this.이름 = "김철수"; // 상속받았기 때문에 선언 없이 사용 가능.
+    Article() {
+        this(lastId + 1, "2025-03-14 12:12:12"); // 반드시 첫번쨰 줄에 작성
+        //다른 생성자 호출(실행), Constructor Call
+        lastId++;
+
+    }
+
+    int id;
+    String regDate;
+
+
+
+    Article(int id, String regDate) {
+        this.id = id;
+
     }
 }
 
 
-// break point 디버그 메인함수로 동작하면 하나씩 코드를 볼 수 있음.
-//기능 참고 : intelliJ break point, debug mode
