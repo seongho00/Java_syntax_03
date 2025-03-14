a97026f444d2d15538668475e2c195f4b113435e
seongho00 <tjdwns000927@gmail.com>
2025-03-14 13:38:02 +0900

    10th_work_02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,65 +10,96 @@ import java.util.Scanner;
 import java.awt.*;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.List;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
+        v4();
+    }
 
-//        int lastID = 0; // 클래스쪽에 집어넣기
-//        Article a1 = new Article();
-//        lastID++;
-//        a1.id = lastID;
-//        System.out.println(a1.id);
-//
-//
-//        Article a2 = new Article();
-//        lastID++;
-//        a2.id = lastID;
-//
-//
-//        Article a3 = new Article(2, "2025-03-14 12:12:12");
-//        lastID++;
-//        a3.id = lastID;
+    static void v4() {
+        System.out.println("==v4==");
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article()); // index : 0 , id : 1
+        articles.add(new Article()); // index : 1 , id : 2
+        articles.add(new Article()); // index : 2 , id : 3
 
-        Article a1 = new Article();
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = articles.get(i);
+            System.out.println(article.id);
+        }
+    }
 
-        Article a2 = new Article();
+    static void v3() {
+        System.out.println("==v3==");
+        ArrayList articles = new ArrayList<>();
+        articles.add(true); // index : 0 , id : 1
+        articles.add(new Article()); // index : 1 , id : 2
+        articles.add(1); // index : 2 , id : 3
+        articles.add(new Article()); // index : 3 , id : 4
 
-        Article[] articles = new Article[3]; // 게시글 3개 다룬다
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = articles.get(i); // ArrayList 에 변수를 넣을 떄는 모든 타입을 넣을 수 있음. (Object 타입)
+            //강제형변환 필요
+            System.out.println(article.id);
+        }
+    }
 
-        articles[0] = a1;
-        articles[1] = a2;
-        System.out.println(articles[0].id);
+    static void v2() {
+        System.out.println("==v2==");
+        ArrayList articles = new ArrayList();
+        articles.add(new Article()); // index : 0 , id : 1
+        articles.add(new Article()); // index : 1 , id : 2
+        articles.add(new Article()); // index : 2 , id : 3
+        articles.add(new Article()); // index : 3 , id : 4
 
-        articles[2] = new Article(); // 배열로 한번에 여러 객체를 다룬다.
+        for (int i = 0; i < articles.size(); i++) {
+            Article article = (Article) articles.get(i);
+            System.out.println(article.id);
+        }
+    }
 
+    static void v1() {
+        System.out.println("==v1==");
+        Article[] articles = new Article[100];
 
+        int articlesSize = 0;
+
+        articles[0] = new Article();
+        articlesSize++;
+        articles[1] = new Article();
+        articlesSize++;
+        articles[2] = new Article();
+        articlesSize++;
+        articles[3] = new Article();
+        articlesSize++;
+        articles[4] = new Article();
+        articlesSize++;
+
+        for (int i = 0; i < articlesSize; i++) {
+            System.out.println(articles[i].id);
+        }
     }
 }
 
 class Article {
-    static int lastId; // 클래스변수(정적변수) (클래스에 속함) : 모든 인스턴스가 공유함
-    //객체생성과 무관 /공공자원
-
-    static { //static 전용 생성자 / 프로그램 시작할 때 바로 작동
-        lastId = 0;
-    }
-
-    Article() {
-        this(lastId + 1, "2025-03-14 12:12:12"); // 반드시 첫번쨰 줄에 작성
-        //다른 생성자 호출(실행), Constructor Call
-        lastId++;
-
-    }
+    static int lastId;
 
     int id;
     String regDate;
 
+    static {
+        lastId = 0;
+    }
 
+    Article() {
+        this(lastId + 1, "2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
+        lastId++;
+    }
 
     Article(int id, String regDate) {
         this.id = id;
-
+        this.regDate = regDate;
     }
 }
 
